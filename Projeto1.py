7d3fbf98c59ebda7bab4c68dfccf5b1c0ed6d9ce
Netto <orlandiomnetto@gmail.com>
2021-11-10 09:09:23 -0300

    upd

--- Projeto1.py
+++ Projeto1.py
@@ -116,95 +116,9 @@ class baseDados:
         # self.connec.commit()
 
 
-    # def remVal(self,cod,qtd,val):
-    #     consulta = 'UPDATE OR IGNORE itens SET qtd=?, val=? WHERE cod=?'
-    #     self.cursor.execute(consulta, (qtd, val, cod))
-    #     self.connec.commit()
-
-
     def fechar(self):
         self.connec.close()
         self.cursor.close()
 
 
 itens = baseDados('itensBaseDados.db')
-# itens.listar()
-# baseDados.getItens(itens)
-#itens.edtVal(628,122224,33)
-
-
-# itens.verificar(628,33)
-# itens.editVal(628,67,'20/01/22','+')
-# print(itens.qtdTotal(628))
-
-# def salvarItem(temp):
-#     with open('itens.csv', 'a', newline='') as arquivo:
-#         escreve = csv.writer(
-#             arquivo,
-#             delimiter=',',  # Delimitador (Vírgula no caso)
-#             quotechar='"',  # Caractere de citação (No caso deixa os valores entre aspas)
-#             quoting=csv.QUOTE_ALL
-#         )
-#         escreve.writerow([temp[0], temp[1], temp[2], temp[3], temp[4]])
-#
-#
-# def addItem(cod,nome,preco,qtd,validade):
-#     temp = []
-#     temp.append(cod)
-#     temp.append(nome)
-#     temp.append(preco)
-#     temp.append(qtd)
-#     temp.append(validade)
-#     salvarItem(temp)
-#     temp.clear()
-#
-#
-# def verLista():
-#     with open('itens.csv', 'r', newline='') as arquivo:
-#         # Necessário a indentação por ser um gerador
-#         # csv.reader = lê como listas
-#         # csv.DictReader = lê como dicionários
-#         #dados = csv.reader(arquivo) #Modo mais comum
-#         dados = [x for x in csv.DictReader(arquivo)]  # Método para não esgotar o uso dos dados
-#         for dado in dados:
-#             print(dado)
-#
-#
-# def to_ascii(ls):
-#     for i in range(len(ls)):
-#         ls[i] = unidecode(ls[i])
-#
-#
-# def buscaItem(key):
-#     #key = unidecode(key)
-#
-#     with open('itens.csv', 'r', newline='') as arquivo:
-#         # Necessário a indentação por ser um gerador
-#         # csv.reader = lê como listas
-#         # csv.DictReader = lê como dicionários
-#         #dados = csv.reader(arquivo) #Modo mais comum
-#         dados = [x for x in csv.reader(arquivo)]  # Método para não esgotar o uso dos dados
-#         cont = 0
-#         results = []
-#         for dado in dados:
-#             #to_ascii(dado)
-#             if re.findall(f'{key}',str(dado), flags=re.I) != []:
-#                 results.append(dados[cont])
-#             cont += 1
-#         return results
-#
-# print(buscaItem('arroz'))
-#
-# def vender(cod,qtd,prc,val):
-#     pass
-#
-# def getPreco():
-#     pass
-#
-#
-
-
-# addItem(1212,'arroz','14,25',32,'24/12/21')
-# verLista()
-
-
